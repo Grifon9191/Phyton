95a13bd396398f02cb109210794e330dfd30ad7d
Grifon9191 <76450149+Grifon9191@users.noreply.github.com>
2020-12-29 23:04:48 +0500

    Update HW_3.3.py

--- HW_3.3.py
+++ HW_3.3.py
@@ -1,10 +1,13 @@
 def my_func(a, b, c):
     global number_list, max_number, max_summ
-    number_list = list((a, b, c)) #переводим три введенных числа в список
-    max_number = max(number_list) #находим максимальное число
-    number_list.remove(max_number) #удаляем максимальное число, в списке остается два числа
-    max_summ = max_number + (max(number_list)) # определяем максимальное число из оставшихся двух и складываем
-    return max_summ
+    if a == b and c:
+        return "ERROR - все числа равны"
+    else:
+        number_list = list((a, b, c)) #переводим три введенных числа в список
+        max_number = max(number_list) #находим максимальное число
+        number_list.remove(max_number) #удаляем максимальное число, в списке остается два числа
+        max_summ = max_number + (max(number_list)) # определяем максимальное число из оставшихся двух и складываем
+        return max_summ
 
 first_number = int(input("1 число:  "))
 second_number = int(input("2 число:  "))
